cec6677e9dd7f81bab37ab10d59651e6a5625825
Zachary Bryant <Zachary@admiralfeb.net>
2020-01-08 22:46:18 -0600

    initial components

---
+++ src/app/components/video-youtube/video-youtube.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoYoutubeComponent } from './video-youtube.component';
+
+
+
+@NgModule({
+  declarations: [VideoYoutubeComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [VideoYoutubeComponent]
+})
+export class VideoYoutubeModule { }
